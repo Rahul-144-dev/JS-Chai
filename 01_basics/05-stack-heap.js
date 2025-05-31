f355d30ffd9067162eefc5d90b63a51e674217aa
RAHUL144 <117508363+rahulsaha1045451444@users.noreply.github.com>
2025-05-31 13:43:54 +0530

    data types

--- 01_basics/05-stack-heap.js
+++ 01_basics/05-stack-heap.js
@@ -1,8 +1,8 @@
 let name1 = "Rahul";
 let name2 = name1;
 name2 = "Surajit";
-// console.log(name1);
-// console.log(name2);
+console.log(name1);
+console.log(name2);
 let user1 =
   {
     name: "Rahul",
@@ -12,3 +12,4 @@ let user2 = user1;
 user2.name = "Changed";
 console.log(user1.name);
 console.log(user2.name);
+
